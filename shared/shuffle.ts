4917c45ceafe6d6d9458e221188aa508da257eea
Alex Coleman <alexrcoleman13@gmail.com>
2026-05-17 06:34:48 -0700

    Use random seed for shuffle RNG

--- shared/shuffle.ts
+++ shared/shuffle.ts
@@ -1,8 +1,13 @@
+import rand from "rand-seed";
+
+const randomSeed = `${Date.now()}-${Math.random()}`;
+const random = new rand(randomSeed);
+
 export default function shuffle<T>(array: T[]): T[] {
   let currentIndex = array.length;
 
   while (currentIndex != 0) {
-    const randomIndex = Math.floor(Math.random() * currentIndex);
+    const randomIndex = Math.floor(random.next() * currentIndex);
     currentIndex--;
 
     [array[currentIndex], array[randomIndex]] = [
